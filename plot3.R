582284f1701692957c61ecba5a0e7be15666231c
unknown <norivicjr@gmail.com>
2014-05-12 01:24:44 +0200

    uploaded solutions

--- plot3.R
+++ plot3.R
@@ -9,9 +9,9 @@ Sys.setlocale(category = "LC_ALL", locale = "")
 ### Plot 3
 
 png(filename="plot3.png", width = 480, height = 480 units = "px")
-plot(plotData$DateTime,as.numeric(type.convert(as.character(plotData$Sub_metering_1), dec = ".")), type = "l", xlab = "", ylab = "Energy sub metering", col = "black")
-points(plotData$DateTime,as.numeric(type.convert(as.character(plotData$Sub_metering_2), dec = ".")), type = "l", xlab = "", ylab = "Energy sub metering", col = "red")
-points(plotData$DateTime,as.numeric(type.convert(as.character(plotData$Sub_metering_3), dec = ".")), type = "l", xlab = "", ylab = "Energy sub metering", col = "blue")
+plot(DatenPlot$DateTime,as.numeric(type.convert(as.character(DatenPlot$Sub_metering_1), dec = ".")), type = "l", xlab = "", ylab = "Energy sub metering", col = "black")
+points(DatenPlot$DateTime,as.numeric(type.convert(as.character(DatenPlot$Sub_metering_2), dec = ".")), type = "l", xlab = "", ylab = "Energy sub metering", col = "red")
+points(DatenPlot$DateTime,as.numeric(type.convert(as.character(DatenPlot$Sub_metering_3), dec = ".")), type = "l", xlab = "", ylab = "Energy sub metering", col = "blue")
 legend("topright", lty = 1, col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 
 dev.off()
